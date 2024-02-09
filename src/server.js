bfb36c5d449382d5dc413f7dede6b3ef885cdf6c
genssauer <genesson_sauer@hotmail.com>
2024-02-09 10:10:01 -0300

    chore: add middlewares

--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
 import http from "node:http";
+import { json } from "./middlewares/json.js";
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url, headers } = req;
 
-  console.log(
-    `Método: ${method} URL: ${url} Headers: ${JSON.stringify(headers)}`
-  );
+  await json(req, res)
 
   if (method === "GET" && url === "/users") {
     return res
-      .setHeader("Content-type", "application/json")
       .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com",
+      name,
+      email,
     });
 
     return res
