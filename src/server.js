3d42d1f1defe0e88ad5ff384cd458dab72225623
genssauer <genesson_sauer@hotmail.com>
2024-02-13 12:52:22 -0300

    chore: implement routes

--- src/server.js
+++ src/server.js
@@ -1,41 +1,27 @@
-import { randomUUID } from 'node:crypto';
 import http from "node:http";
-import { Database } from "./database.js";
 import { json } from "./middlewares/json.js";
-
-const database = new Database()
+import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 const server = http.createServer(async (req, res) => {
-  const { method, url, headers } = req;
+  const { method, url } = req;
 
-  await json(req, res)
+  await json(req, res);
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select('users')
+  const route = routes.find(route => route.method === method && route.path.test(url));
 
-    return res
-      .end(JSON.stringify(users));
+  if (route) {
+    const routeParams = req.url.match(route.path);
+
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
+
+    return route.handler(req, res);
   }
 
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    };
-
-    database.insert('users', user)
-
-    return res
-      .writeHead(201)
-      .end();
-  }
-
-  return res
-    .writeHead(404)
-    .end();
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
